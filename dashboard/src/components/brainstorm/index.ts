3078ffc670d7e4bec3e9a3b32fc89d8fecd0c016
Kevin Anderson <anderskev@gmail.com>
2026-01-18 16:18:47 -0500

    feat(dashboard): add brainstorm components barrel export

---
+++ dashboard/src/components/brainstorm/index.ts
@@ -0,0 +1,5 @@
+export { SessionListItem } from "./SessionListItem";
+export { SessionDrawer } from "./SessionDrawer";
+// TODO: Add these exports when components are implemented (Tasks 7-8)
+// export { ArtifactCard } from "./ArtifactCard";
+// export { HandoffDialog } from "./HandoffDialog";
