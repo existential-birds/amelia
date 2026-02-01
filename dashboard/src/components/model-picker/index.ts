d3735abd4b3f8aff18134fb778c16be0c02a2892
Kevin Anderson <anderskev@gmail.com>
2026-02-01 18:33:48 -0500

    feat(model-picker): add barrel export for model-picker components

---
+++ dashboard/src/components/model-picker/index.ts
@@ -0,0 +1,15 @@
+// dashboard/src/components/model-picker/index.ts
+
+export { ApiModelSelect } from './ApiModelSelect';
+export { ModelPickerSheet } from './ModelPickerSheet';
+export { ModelList } from './ModelList';
+export { ModelListItem } from './ModelListItem';
+export { ModelSearchFilters } from './ModelSearchFilters';
+export type { ModelInfo, AgentRequirements, PriceTier } from './types';
+export {
+  AGENT_MODEL_REQUIREMENTS,
+  PRICE_TIER_THRESHOLDS,
+  RECENT_MODELS_KEY,
+  MAX_RECENT_MODELS,
+  MODELS_API_URL,
+} from './constants';
