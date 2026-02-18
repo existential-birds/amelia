5cdeca6136628c3ab64197dabe6257851a5be8a3
Kevin Anderson <anderskev@gmail.com>
2026-02-17 22:17:17 -0500

    fix: configure ESLint to ignore caught errors with underscore prefix
    - Add caughtErrorsIgnorePattern to ignore caught errors prefixed with underscore
    - Set caughtErrors to "all" to enforce pattern for all caught errors
    - Resolves ESLint warnings for intentionally unused error variables
    
    Iteration: 3

--- dashboard/eslint.config.js
+++ dashboard/eslint.config.js
@@ -23,6 +23,8 @@ export default tseslint.config(
         {
           argsIgnorePattern: "^_",
           varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+          caughtErrors: "all",
         },
       ],
     },
